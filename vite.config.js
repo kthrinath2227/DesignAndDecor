8494cf0d6f8a39cdb37fd739bee3fb6948d6ce33
kthrinath2227 <k.thrinath2227@gmail.com>
2025-09-27 17:07:10 +0530

    tesing

--- vite.config.js
+++ vite.config.js
@@ -191,7 +191,7 @@ logger.error = (msg, options) => {
 
 export default defineConfig({
 	plugins: [react()],
-  base: '/',
+  base: './',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
